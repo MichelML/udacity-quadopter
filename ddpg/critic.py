490154040698fe94868b4de9890077ee6948a8e6
michelml <michmoreau.l@gmail.com>
2019-06-11 18:08:32 +0000

    add discounts to state
    The z discount was converted to the same discount function as the two other velocities.
    We also lowered the target from 100 to 30 for z position, as it will train faster.

--- ddpg/critic.py
+++ ddpg/critic.py
@@ -58,7 +58,7 @@ class Critic:
         self.model = models.Model(inputs=[states, actions], outputs=Q_values)
 
         # Define optimizer and compile model for training with built-in loss function
-        optimizer = optimizers.Adam(lr=0.00001)
+        optimizer = optimizers.Adam(lr=0.001)
         self.model.compile(optimizer=optimizer, loss='mse')
 
         # Compute action gradients (derivative of Q values w.r.t. to actions)
