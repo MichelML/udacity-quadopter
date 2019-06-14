2a34c5acc934bfe3d227b90095fb076da89abf83
michelml <michmoreau.l@gmail.com>
2019-06-14 19:38:18 +0000

    added splitted discounts and rewards plotting
    it is clearer now how discoutns and rewards interact with each other. The next step will be to identify in which ways we can incentivize the drone to "deccelerate" when approaching the targeted z value

--- task2.py
+++ task2.py
@@ -46,41 +46,48 @@ class Task():
     def get_dist_between_points(self, pos, target):
         return distance.cdist([pos], [target])[0][0]
 
-    def get_reward(self, rotor_speeds):
-        reward_z = min(self.sim.pose[2], self.target_pos[2]) if self.sim.pose[2] <= self.target_pos[2] else -1.
+    def get_net_reward(self, rotor_speeds):
+        splitted_rewards, total_reward = self.get_rewards()
+        splitted_discounts, total_discount = self.get_discounts()
+
+        return splitted_rewards, splitted_discounts, total_reward, total_discount, total_reward - total_discount
+
+    def get_rewards(self):
+        z_reward = min(self.sim.pose[2], self.target_pos[2]) if self.sim.pose[2] <= self.target_pos[2] else 0.
         time_reward = 2.
-        total_reward = sum([reward_z, time_reward])
+        total_reward = sum([z_reward, time_reward])
         
+        return [z_reward, time_reward], total_reward
+
+    def get_discounts(self):
         xy_displacement_discount = 0.001*sum(np.square(np.array(self.sim.pose[:2]) - np.array(self.target_pos[:2])))
         xy_velocity_discount = 0.001*sum(np.square(self.sim.v[:2]))
         angular_velocity_discount = 0.001*sum(np.square(self.sim.angular_v))
         euler_angles_discount = 0.01*self.get_dist_between_points(self.sim.pose[3:], self.target_pos[3:])
         total_discount = sum([xy_displacement_discount, xy_velocity_discount, angular_velocity_discount, euler_angles_discount])
-#         print(f'xy {xy_displacement_discount}')
-#         print(f'xy v {xy_velocity_discount}')
-#         print(f'angular_v {angular_velocity_discount}')
-#         print(f'euler {euler_angles_discount}')
-#         print('----------')
-
-        return total_reward - total_discount
+        
+        return [xy_displacement_discount, xy_velocity_discount, angular_velocity_discount, euler_angles_discount], total_discount
     
     def should_terminate(self):
-#         if self.sim.pose[2] > self.target_pos[2] + 2.:
-#             return True
+        if self.sim.pose[2] > self.target_pos[2] + 2.:
+            return True
         return False
 
     def step(self, rotor_speeds):
         """Uses action to obtain next state, reward, done."""
         reward = 0
+        splitted_r = np.zeros(9)
         pose_all = []
         for _ in range(self.action_repeat):
             # update the sim pose and velocities
             done = self.sim.next_timestep(rotor_speeds) or self.should_terminate()
-            reward += self.get_reward(rotor_speeds)
+            splitted_rewards, splitted_discounts, total_reward, total_discount, net_reward = self.get_net_reward(rotor_speeds)
+            splitted_r = splitted_r + np.concatenate([splitted_rewards, splitted_discounts, [total_reward, total_discount, net_reward]])
+            reward += net_reward
             self.score += reward
             pose_all.append(self.get_sim_state())
         next_state = np.concatenate(pose_all)
-        return next_state, reward, done
+        return next_state, splitted_r, done
 
     def reset(self):
         if self.score > self.best_score:
@@ -94,4 +101,4 @@ class Task():
         return state
     
     def get_sim_state(self):
-        return np.concatenate([self.sim.pose, self.sim.v, self.sim.angular_v, self.sim.linear_accel, self.sim.angular_accels])
+        return np.concatenate([self.sim.pose, self.sim.v, self.sim.angular_v, self.sim.linear_accel, self.sim.angular_accels, [self.sim.time]])
